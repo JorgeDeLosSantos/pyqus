cf2b04cc1545b4d053a992643030eceae031673c
Jorge De Los Santos <delossantosmfq@gmail.com>
2016-01-10 16:46:37 -0600

    Setup.py created

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='pyqus',
+      version='0.1.0',
+      description='Abaqus OBD files postprocessing',
+      author='LABDLS',
+      author_email='delossantosmfq@gmail.com',
+      license = "MIT",
+      url='https://github.com/JorgeDeLosSantos/pyqus',
+      packages=['pyqus'],
+     )
