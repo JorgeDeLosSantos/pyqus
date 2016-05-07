770cda14f9814af34014c747d4c4c0d61f921a3d
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-05-06 23:49:38 -0500

    distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='pyqus',
-      version='0.1.0',
-      description='Abaqus OBD files postprocessing',
-      author='Pedro Jorge De Los Santos',
-      author_email='delossantosmfq@gmail.com',
-      license = "MIT",
-      url='https://github.com/JorgeDeLosSantos/pyqus',
-      packages=['pyqus'],
-     )
+setup(
+    name="pyqus",
+    version='0.1.0-dev',
+    description='Abaqus OBD files postprocessing',
+    author='Pedro Jorge De Los Santos',
+    author_email='delossantosmfq@gmail.com',
+    license = "MIT",
+    keywords=["FEA","Abaqus","Postprocessing"],
+    install_requires=["matplotlib"],
+    url='https://github.com/JorgeDeLosSantos/pyqus',
+    packages=['pyqus']
+)
